d13d54f816e4a44ac794d6d3419290c1fc220ffd
Mingzhi Lin <mingzhilin@google.com>
2019-03-29 17:04:23 -0400

    Added rule in-line disabling.

--- rules/core.go
+++ rules/core.go
@@ -4,6 +4,7 @@ package rules
 
 import (
 	"log"
+	"strings"
 
 	"github.com/golang/protobuf/reflect/protoreflect"
 	"github.com/jgeewax/api-linter/lint"
@@ -20,7 +21,7 @@ func registerRuleWithLintFunc(metadata ruleMetadata, lintFunc lintFuncType) {
 	registerRuleBase(r)
 }
 
-func registerRuleWithVisitor(metadata ruleMetadata, visitor protoVisitor) {
+func registerRuleWithVisitor(metadata ruleMetadata, visitor protoRuleVisitor) {
 	r := ruleBase{
 		metadata: metadata,
 		visitor:  visitor,
@@ -55,7 +56,7 @@ type lintFuncType func(lint.Request) (lint.Response, error)
 type ruleBase struct {
 	metadata ruleMetadata
 	lintFunc lintFuncType
-	visitor  protoVisitor
+	visitor  protoRuleVisitor
 }
 
 func (r ruleBase) ID() lint.ID {
@@ -86,13 +87,13 @@ func (r ruleBase) Lint(req lint.Request) (lint.Response, error) {
 		return r.lintFunc(req)
 	}
 	if r.visitor != nil {
-		return r.visitor.Walk(req)
+		return r.visitor.Walk(r, req)
 	}
 	return lint.Response{}, nil
 }
 
-type protoVisitor interface {
-	Walk(req lint.Request) (lint.Response, error)
+type protoRuleVisitor interface {
+	Walk(r lint.Rule, req lint.Request) (lint.Response, error)
 }
 
 // simpleVisitor can check top level enums, all messages and services.
@@ -105,11 +106,13 @@ type simpleVisitor struct {
 	ServiceCheck   func(protoreflect.ServiceDescriptor, lint.Context) []lint.Problem
 	MethodCheck    func(protoreflect.MethodDescriptor, lint.Context) []lint.Problem
 
+	rule     lint.Rule
 	ctx      lint.Context
 	problems []lint.Problem
 }
 
-func (v *simpleVisitor) Walk(req lint.Request) (lint.Response, error) {
+func (v *simpleVisitor) Walk(r lint.Rule, req lint.Request) (lint.Response, error) {
+	v.rule = r
 	v.ctx = req.Context()
 	v.checkAllMessages(req.ProtoFile())
 	v.checkTopLevelEnums(req.ProtoFile())
@@ -118,39 +121,40 @@ func (v *simpleVisitor) Walk(req lint.Request) (lint.Response, error) {
 }
 
 func (v *simpleVisitor) checkMessage(m protoreflect.MessageDescriptor) {
-	if v.MessageCheck != nil {
+	if v.MessageCheck != nil && v.isRuleEnabled(m) {
 		v.addProblems(v.MessageCheck(m, v.ctx)...)
 	}
 }
 
 func (v *simpleVisitor) checkField(f protoreflect.FieldDescriptor) {
-	if v.FieldCheck != nil {
+	if v.FieldCheck != nil && v.isRuleEnabled(f) {
 		v.addProblems(v.FieldCheck(f, v.ctx)...)
 	}
 }
 
 func (v *simpleVisitor) checkEnum(e protoreflect.EnumDescriptor) {
-	if v.EnumCheck != nil {
+	if v.EnumCheck != nil && v.isRuleEnabled(e) {
 		v.addProblems(v.EnumCheck(e, v.ctx)...)
 	}
 }
 
 func (v *simpleVisitor) checkEnumValue(ev protoreflect.EnumValueDescriptor) {
-	if v.EnumValueCheck != nil {
+	if v.EnumValueCheck != nil && v.isRuleEnabled(ev) {
 		v.addProblems(v.EnumValueCheck(ev, v.ctx)...)
 	}
 }
 
 func (v *simpleVisitor) checkOneof(o protoreflect.OneofDescriptor) {
-	if v.EnumCheck != nil {
+	if v.EnumCheck != nil && v.isRuleEnabled(o) {
 		v.addProblems(v.OneofCheck(o, v.ctx)...)
 	}
 }
 
 func (v *simpleVisitor) checkTopLevelEnums(f protoreflect.FileDescriptor) {
-	if v.EnumCheck != nil {
-		for i := 0; i < f.Enums().Len(); i++ {
-			v.addProblems(v.EnumCheck(f.Enums().Get(i), v.ctx)...)
+	for i := 0; i < f.Enums().Len(); i++ {
+		e := f.Enums().Get(i)
+		if v.EnumCheck != nil && v.isRuleEnabled(e) {
+			v.addProblems(v.EnumCheck(e, v.ctx)...)
 		}
 	}
 }
@@ -176,18 +180,53 @@ func (v *simpleVisitor) checkAllServices(f protoreflect.FileDescriptor) {
 	})
 }
 
-func (v *simpleVisitor) checkService(n protoreflect.ServiceDescriptor) {
-	if v.ServiceCheck != nil {
-		v.addProblems(v.ServiceCheck(n, v.ctx)...)
+func (v *simpleVisitor) checkService(s protoreflect.ServiceDescriptor) {
+	if v.ServiceCheck != nil && v.isRuleEnabled(s) {
+		v.addProblems(v.ServiceCheck(s, v.ctx)...)
 	}
 }
 
 func (v *simpleVisitor) checkMethod(m protoreflect.MethodDescriptor) {
-	if v.MethodCheck != nil {
+	if v.MethodCheck != nil && v.isRuleEnabled(m) {
 		v.addProblems(v.MethodCheck(m, v.ctx)...)
 	}
 }
 
 func (v *simpleVisitor) addProblems(problems ...lint.Problem) {
 	v.problems = append(v.problems, problems...)
+}
+
+func (v *simpleVisitor) isRuleEnabled(d protoreflect.Descriptor) bool {
+	return isRuleEnabled(v.rule.ID(), d, v.ctx)
+}
+
+func isRuleEnabled(ruleID lint.ID, d protoreflect.Descriptor, ctx lint.Context) bool {
+	descSource, err := ctx.DescriptorSource()
+	if err != nil {
+		return true
+	}
+	comments, err := descSource.FindCommentsByDescriptor(d)
+	if err != nil {
+		return true
+	}
+
+	leadingAndInLineComments := []string{comments.LeadingComments, comments.TrailingComments}
+	return !stringsContains(leadingAndInLineComments, ruleDisablingComment(ruleID))
+}
+
+func stringsContains(comments []string, s string) bool {
+	for _, c := range comments {
+		if strings.Contains(c, s) {
+			return true
+		}
+	}
+	return false
+}
+
+func ruleDisablingComment(id lint.ID) string {
+	name := id.Set + "." + id.Name
+	if id.Set == "" || id.Set == "core" {
+		name = id.Name
+	}
+	return "(-- api-linter: " + name + "=disabled --)"
 }
