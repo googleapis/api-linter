71839418be8f0772c7e92a87058ca2a8b5399fa7
Mingzhi Lin <mingzhilin@google.com>
2019-04-11 18:06:46 -0400

    Added getters for Request and creating a Request from file descriptor proto.

--- lint/request.go
+++ lint/request.go
@@ -1,17 +1,37 @@
 package lint
 
-import "github.com/golang/protobuf/v2/reflect/protoreflect"
+import (
+	"github.com/golang/protobuf/v2/reflect/protodesc"
+	"github.com/golang/protobuf/v2/reflect/protoreflect"
+	descriptorpb "github.com/golang/protobuf/v2/types/descriptor"
+)
 
 // Request defines input data for a rule to perform linting.
 type Request struct {
-	protoFile  protoreflect.FileDescriptor
+	fileDesc   protoreflect.FileDescriptor
 	descSource DescriptorSource
 }
 
-// NewProtoFileRequest creates a linting request for a .proto file.
-func NewProtoFileRequest(protoFile protoreflect.FileDescriptor, descSource DescriptorSource) Request {
-	return Request{
-		protoFile:  protoFile,
-		descSource: descSource,
+// ProtoFile returns a FileDescriptor of the .proto file that will be linted.
+func (r Request) ProtoFile() protoreflect.FileDescriptor {
+	return r.fileDesc
+}
+
+// DescriptorSource returns a DescriptorSource that contains additional source
+// information for the .proto file that will be linted.
+func (r Request) DescriptorSource() DescriptorSource {
+	return r.descSource
+}
+
+// NewProtoFileRequest creates a linting Request for a .proto file.
+func NewProtoFileRequest(fd *descriptorpb.FileDescriptorProto) (Request, error) {
+	f, err := protodesc.NewFile(fd, nil)
+	if err != nil {
+		return Request{}, err
 	}
+	s, err := NewDescriptorSource(fd)
+	return Request{
+		fileDesc:   f,
+		descSource: s,
+	}, err
 }
