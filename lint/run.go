76a6464afd78da54677b10c43000c08808d92734
Mingzhi Lin <mingzhi9@gmail.com>
2019-04-14 17:48:40 -0400

    Removed the rule set notation.
    Instead, rules can be grouped into module in the name notation.
    For example, rule "a.b" and "a.c" are belonged to the same module "a".

--- lint/run.go
+++ lint/run.go
@@ -10,11 +10,6 @@ func Run(rules Rules, request Request) (Response, error) {
 	return run(rules.All(), request)
 }
 
-// RunWithConfig invokes rules filtered by the `RulesConfig` on the request.
-func RunWithConfig(rules Rules, request Request, cfg RulesConfig) (Response, error) {
-	return run(rules.FindByConfig(cfg), request)
-}
-
 func run(rules []Rule, req Request) (Response, error) {
 	finalResp := Response{}
 	errMessages := []string{}
