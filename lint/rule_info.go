5e2bc142dc5c915eb273af87b26610861fa8253a
Mingzhi Lin <mingzhilin@google.com>
2019-04-17 11:56:31 -0400

    Converted RuleInfo from a struct to an interface.

--- lint/rule_info.go
+++ lint/rule_info.go
@@ -1,7 +1,16 @@
 package lint
 
+// RuleInfo defines meta-information for a Rule.
+type RuleInfo interface {
+	Name() string          // returns the rule name.
+	Description() string   // returns a short description of this rule.
+	URL() string           // returns a link to a document for more details.
+	FileTypes() []FileType // returns the list of FileType that this rule is targeting to.
+	Category() Category    // returns the Category this rule will produce.
+}
+
 // ruleInfo stores information of a rule.
-type RuleInfo struct {
+type ruleInfo struct {
 	name        string     // rule name in the set.
 	description string     // a short description of this rule.
 	url         string     // a link to a document for more details.
@@ -9,8 +18,9 @@ type RuleInfo struct {
 	category    Category   // category of problems this rule produces.
 }
 
+// NewRuleInfo creates and returns a RuleInfo from the provided information.
 func NewRuleInfo(name, description, url string, fileTypes []FileType, category Category) RuleInfo {
-	return RuleInfo{
+	return ruleInfo{
 		name:        name,
 		description: description,
 		url:         url,
@@ -19,22 +29,22 @@ func NewRuleInfo(name, description, url string, fileTypes []FileType, category C
 	}
 }
 
-func (r RuleInfo) Name() string {
+func (r ruleInfo) Name() string {
 	return r.name
 }
 
-func (r RuleInfo) Description() string {
+func (r ruleInfo) Description() string {
 	return r.description
 }
 
-func (r RuleInfo) URL() string {
+func (r ruleInfo) URL() string {
 	return r.url
 }
 
-func (r RuleInfo) FileTypes() []FileType {
+func (r ruleInfo) FileTypes() []FileType {
 	return r.fileTypes
 }
 
-func (r RuleInfo) Category() Category {
+func (r ruleInfo) Category() Category {
 	return r.category
 }
