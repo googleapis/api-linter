f3d664a9b516fc161fe72ed39fd0c746d45390b2
Mingzhi Lin <mingzhi9@gmail.com>
2019-04-19 16:37:43 -0400

    Refactor.

--- rules/testdata/test_helpers.go
+++ rules/testdata/test_helpers.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -17,22 +18,20 @@ import (
 
 var protoc = "protoc"
 
-func descriptorProtoFromSource(source []byte) (*descriptorpb.FileDescriptorProto, error) {
+func descriptorProtoFromSource(source io.Reader) (*descriptorpb.FileDescriptorProto, error) {
 	tmpDir := os.TempDir()
 
 	f, err := ioutil.TempFile(tmpDir, "proto*")
-
 	if err != nil {
 		return nil, err
 	}
 	defer mustCloseAndRemoveFile(f)
 
-	if _, err = f.Write(source); err != nil {
+	if _, err = io.Copy(f, source); err != nil {
 		return nil, err
 	}
 
 	descSetF, err := ioutil.TempFile(tmpDir, "descset*")
-
 	if err != nil {
 		return nil, err
 	}
@@ -46,16 +45,14 @@ func descriptorProtoFromSource(source []byte) (*descriptorpb.FileDescriptorProto
 		f.Name(),
 	)
 
-	var stdErrBuf bytes.Buffer
-
-	cmd.Stderr = &stdErrBuf
+	stderr := new(bytes.Buffer)
+	cmd.Stderr = stderr
 
 	if err = cmd.Run(); err != nil {
-		return nil, fmt.Errorf("protoc failed with %v and Stderr %q", err, stdErrBuf.String())
+		return nil, fmt.Errorf("protoc failed with %v and Stderr %q", err, stderr.String())
 	}
 
 	descSet, err := ioutil.ReadFile(descSetF.Name())
-
 	if err != nil {
 		return nil, err
 	}
@@ -66,7 +63,7 @@ func descriptorProtoFromSource(source []byte) (*descriptorpb.FileDescriptorProto
 	}
 
 	if len(protoset.GetFile()) == 0 {
-		return nil, fmt.Errorf("protoset file list was empty")
+		return nil, fmt.Errorf("protoset.GetFile() returns empty list")
 	}
 
 	return protoset.GetFile()[0], nil
@@ -90,14 +87,16 @@ func MustCreateTemplate(tmpl string) *template.Template {
 
 // MustCreateRequestFromTemplate creates a lint.Request from the provided template and test data.
 func MustCreateRequestFromTemplate(tmpl *template.Template, testData interface{}) lint.Request {
-	var b bytes.Buffer
-	if err := tmpl.Execute(&b, testData); err != nil {
+	b := new(bytes.Buffer)
+	if err := tmpl.Execute(b, testData); err != nil {
 		log.Fatalf("Error executing template %v", err)
 	}
-	pd, err := descriptorProtoFromSource(b.Bytes())
+
+	pd, err := descriptorProtoFromSource(b)
 	if err != nil {
 		log.Fatalf("Error generating proto descriptor: %v", err)
 	}
+
 	req, err := lint.NewProtoFileRequest(pd)
 	if err != nil {
 		log.Fatalf("Error creating proto file request: %v", err)
