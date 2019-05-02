4cfaa98a260e48ab87d53416ac549adeb99bd2a5
robcapo <robcapo@google.com>
2019-05-02 10:49:46 -0400

    Added rule to enforce that the proto package must match the file path

---
+++ rules/corp/proto_package_must_match_file_path.go
@@ -0,0 +1,57 @@
+package corp
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/v2/reflect/protoreflect"
+	"github.com/jgeewax/api-linter/lint"
+	"github.com/jgeewax/api-linter/rules/descriptor"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func protoPackageMustMatchFilePath() lint.Rule {
+	return &descriptor.CallbackRule{
+		RuleInfo: lint.RuleInfo{
+			Name:         "corp::proto_package_must_match_file_path",
+			Description:  "All protos must belong to a package that matches their file path (relative to google3/)",
+			URI:          "http://go/ce-api-conformance-checks",
+			RequestTypes: []lint.RequestType{lint.ProtoRequest},
+		},
+		Callback: descriptor.Callbacks{
+			FileCallback: func(fileDescriptor protoreflect.FileDescriptor, source lint.DescriptorSource) (problems []lint.Problem, e error) {
+				packageFromPath := pathToPackage(fileDescriptor.Path())
+
+				if packageFromPath == "" {
+					return []lint.Problem{{
+						Message:    "Failed to process file path. Please make sure it is in a subdirectory of google3 and ends with .proto",
+						Descriptor: fileDescriptor,
+					}}, nil
+				}
+
+				if string(fileDescriptor.Package()) != packageFromPath {
+					return []lint.Problem{{
+						Message: fmt.Sprintf(
+							"The file path %q must match the package path %q (with path separators replaced by dots",
+							fileDescriptor.Path(), fileDescriptor.Package().Name()),
+						Descriptor: fileDescriptor,
+					}}, nil
+				}
+
+				return nil, nil
+			},
+		},
+	}
+}
+
+var protoPathRegexp = regexp.MustCompile(`^(?:[^/]+/)*?google3/(.*)/[^/]+\.proto$`)
+
+func pathToPackage(path string) string {
+	extractedPath := protoPathRegexp.FindStringSubmatch(path)
+
+	if len(extractedPath) < 2 {
+		return ""
+	}
+
+	return strings.Replace(extractedPath[1], string(os.PathSeparator), ".", -1)
+}
---
+++ rules/corp/proto_package_must_match_file_path_test.go
@@ -0,0 +1,63 @@
+package corp
+
+import (
+	descriptorpb "github.com/golang/protobuf/v2/types/descriptor"
+	"github.com/jgeewax/api-linter/lint"
+	"testing"
+)
+
+func TestProtoPackageMustMatchFilePath(t *testing.T) {
+	rule := protoPackageMustMatchFilePath()
+
+	tests := []struct {
+		path         string
+		protoPackage string
+		numProblems  int
+	}{
+		{"google3/a/b/c/v1/foo.proto", "a.b.c.v1", 0},
+		{"google3/google/corp/a/b/c/d/e/f/alpha/foo.proto", "google.corp.a.b.c.d.e.f.alpha", 0},
+		{"google3/google/corp/a/b/c/d/e/f/beta/foo.proto", "google.corp.a.b.c.d.e.f.beta", 0},
+		{"google3/google/corp/a/b/c/foo.proto", "google.corp.a.b.c", 0},
+
+		// The rule shouldn't break if someone decides to use a subdir named google3
+		{"google3/a/b/google3/foo.proto", "a.b.google3", 0},
+		{"a/b/google3/c/d/google3/bar/baz.proto", "c.d.google3.bar", 0},
+
+		// extension not .proto
+		{"google3/google/corp/a/foo.proto2", "google.corp.a", 1},
+
+		// not in google3/
+		{"a/b/c/d/e.proto", "a.b.c.d", 1},
+
+		// should be "a.b.c"
+		{"google3/a/b/c/d.proto", "a.b.c.d", 1},
+		{"google3/a/b/c/d.proto", "a.b.d", 1},
+		{"google3/a/b/c/d.proto", "a.b", 1},
+	}
+
+	for _, test := range tests {
+
+		req, err := lint.NewProtoRequest(&descriptorpb.FileDescriptorProto{
+			Name:           &test.path,
+			Package:        &test.protoPackage,
+			SourceCodeInfo: &descriptorpb.SourceCodeInfo{},
+		})
+
+		if err != nil {
+			t.Errorf("Failed to create proto request because %v", err)
+		}
+
+		p, err := rule.Lint(req)
+
+		if err != nil {
+			t.Errorf("Lint() on file %q returned an error: %v", test.path, err)
+		}
+
+		if len(p.Problems) != test.numProblems {
+			t.Errorf(
+				"Lint() on file %q returned %d problems; want %d. Problems: %+v",
+				test.path, len(p.Problems), test.numProblems, p.Problems,
+			)
+		}
+	}
+}
