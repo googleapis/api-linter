0aac08348ca185d8abc3b27bc86be5dfe1f00360
robcapo <robcapo@google.com>
2019-05-14 10:32:23 -0400

    Fixed import order

--- rules/check_field_names_use_lower_snake_case.go
+++ rules/check_field_names_use_lower_snake_case.go
@@ -16,12 +16,12 @@ package rules
 
 import (
 	"fmt"
-	"github.com/stoewer/go-strcase"
 	"strings"
 
 	pref "github.com/golang/protobuf/v2/reflect/protoreflect"
 	"github.com/googleapis/api-linter/lint"
 	"github.com/googleapis/api-linter/rules/descriptor"
+	"github.com/stoewer/go-strcase"
 )
 
 func init() {
