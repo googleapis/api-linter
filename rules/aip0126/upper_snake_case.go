814a81b767fa1c2aedcde559804c149748b293f6
Mingzhi Lin <mingzhi9@gmail.com>
2019-09-17 22:10:44 -0400

    Added enum value UPPER_SNAKE rule.

---
+++ rules/aip0126/upper_snake_case.go
@@ -0,0 +1,33 @@
+package aip0126
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/googleapis/api-linter/lint"
+	"github.com/jhump/protoreflect/desc"
+	"github.com/stoewer/go-strcase"
+)
+
+// All enum values must use UPPER_SNAKE_CASE.
+var enumValueUpperSnakeCase = &lint.EnumRule{
+	Name: lint.NewRuleName("core", "0126", "lower-snake"),
+	URI:  "https://aip.dev/126#guidance",
+	LintEnum: func(e *desc.EnumDescriptor) []lint.Problem {
+		var problems []lint.Problem
+		for _, v := range e.GetValues() {
+			if got, want := v.GetName(), toUpperSnakeCase(v.GetName()); got != want {
+				problems = append(problems, lint.Problem{
+					Message:    fmt.Sprintf("Enum value %q must use UPPER_SNAKE_CASE.", got),
+					Suggestion: want,
+					Descriptor: v,
+				})
+			}
+		}
+		return problems
+	},
+}
+
+func toUpperSnakeCase(s string) string {
+	return strings.ToUpper(strcase.SnakeCase(s))
+}
