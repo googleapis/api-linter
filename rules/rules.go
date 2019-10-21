331e691c56bfe0c46fb36a008f377607eea5b447
Luke Sneeringer <lukesneeringer@google.com>
2019-10-21 10:14:36 -0700

    [docs] Add documentation.

--- rules/rules.go
+++ rules/rules.go
@@ -12,7 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package rules contains implementations of rules that apply to all Google APIs.
+// Package rules contains implementations of rules that apply to Google APIs.
+//
+// Rules are sorted into subpackages by the AIP (https://aip.dev/) that
+// mandates the rule. Every rule represented in code here must be represented
+// in English in a corresponding AIP. Conversely, anything mandated in an AIP
+// should have a rule here if it is feasible to enforce in code (sometimes it
+// is infeasible, however).
+//
+// A rule is technically anything with a `GetName()`, `GetURI()``, and
+// `Lint(*desc.FileDescriptorProto) []lint.Problem` method, but most rule
+// authors will want to use the rule structs provided in the lint package
+// (`&lint.MessageRule`, `&lint.FieldRule`, and so on). These run against
+// each applicable descriptor in the file (`MessageRule` against every message,
+// for example). They also have an `OnlyIf` property that can be used to run
+// against a subset of descriptors.
+//
+// A simple rule therefore looks like this:
+//
+//   var myRule = &lint.MessageRule{
+//	   Name: lint.NewRuleName("core", "1234", "my-rule"),
+//     URI:  "https://aip.dev/1234",
+//     LintMessage: func(m *desc.MessageDescriptor) []lint.Problem {
+//	     if isBad(m) {
+//         return []lint.Problem{{
+//           Message: "This message is bad.",
+//           Descriptor: m,
+//         }}
+//       }
+//       return nil
+//     },
+//   }
+//
+// Once a rule is written, it needs to be registered. This involves adding
+// the rule to the `AddRules` method for the appropriate AIP package.
+// If this is the first rule for a new AIP, then the `rules.go` init() function
+// must also be updated to run the `AddRules` function for the new package.
 package rules
 
 import (
