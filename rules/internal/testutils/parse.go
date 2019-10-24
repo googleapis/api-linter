0c0f0a6ebca970bc6ba4bcd2eb9bb29ce89fbb78
Mingzhi Lin <mingzhi@users.noreply.github.com>
2019-10-24 14:19:53 -0700

    [refactor] Use `LookupImport` for common protos. (#258)

--- rules/internal/testutils/parse.go
+++ rules/internal/testutils/parse.go
@@ -16,18 +16,13 @@ package testutils
 
 import (
 	"bytes"
-	"compress/gzip"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"strings"
 	"testing"
 
-	"github.com/golang/protobuf/proto"
-	dpb "github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/desc/protoparse"
-	"github.com/jhump/protoreflect/desc/protoprint"
 	"github.com/lithammer/dedent"
 
 	// These imports cause the common protos to be registered with
@@ -51,14 +46,12 @@ func ParseProtoString(t *testing.T, src string) *desc.FileDescriptor {
 	fileContents := map[string]string{
 		"test.proto": strings.TrimSpace(dedent.Dedent(src)),
 	}
-	for fn, c := range commonProtos {
-		fileContents[fn] = c
-	}
 
 	// Parse the file.
 	parser := protoparse.Parser{
 		Accessor:              protoparse.FileContentsFromMap(fileContents),
 		IncludeSourceCodeInfo: true,
+		LookupImport:          desc.LoadFileDescriptor,
 	}
 	fds, err := parser.ParseFiles("test.proto")
 	if err != nil {
@@ -101,85 +94,4 @@ func ParseProto3Tmpl(t *testing.T, src string, data interface{}) *desc.FileDescr
 
 	// Parse the proto as a string.
 	return ParseProto3String(t, protoBytes.String())
-}
-
-// decompile reads compiled Go protos and returns a map of strings
-// corresponding to the original protos.
-//
-// This is used to handle google.api common protos, which some tests need
-// to use.
-func decompile(filenames ...string) (map[string]string, error) {
-	var fdps []*dpb.FileDescriptorProto
-	for _, filename := range filenames {
-		// Compiled golang protos include a compressed descriptor, which must be
-		// uncompressed before it can be populated into a FileDescriptorProto.
-		compressedDesc := proto.FileDescriptor(filename)
-		r, err := gzip.NewReader(bytes.NewReader(compressedDesc))
-		if err != nil {
-			return nil, err
-		}
-		descBytes, err := ioutil.ReadAll(r)
-		if err != nil {
-			return nil, err
-		}
-
-		// Unmarshal the byte string into a FileDescriptorProto.
-		fdp := &dpb.FileDescriptorProto{}
-		if err := proto.Unmarshal(descBytes, fdp); err != nil {
-			return nil, err
-		}
-		fdps = append(fdps, fdp)
-	}
-	fds, err := desc.CreateFileDescriptors(fdps)
-	if err != nil {
-		return nil, fmt.Errorf(dedent.Dedent(`
-			Unable to create protoreflect descriptors
-			(missing import in args to decompile?): %v
-		`), err)
-	}
-
-	// Iterate over the values in the map and populate the original contents
-	// of the protos.
-	printer := &protoprint.Printer{}
-	answer := make(map[string]string)
-	for filename, fd := range fds {
-		protoStr, err := printer.PrintProtoToString(fd)
-		if err != nil {
-			return nil, err
-		}
-		answer[filename] = protoStr
-	}
-
-	// Done; return the answer.
-	return answer, nil
-}
-
-// Common protos likely to need to be imported in tests.
-// It is okay to add more here; however, this list **must not** include any
-// protos outside of what is defined in AIP-213 (https://aip.dev/213).
-//
-// Note that any file that needs to be included here must also have each
-// of its imports here, and the corresponding compiled Go protos must be
-// imported up top.
-var commonProtos map[string]string
-
-func init() {
-	cp, err := decompile(
-		"google/api/annotations.proto",
-		"google/api/client.proto",
-		"google/api/field_behavior.proto",
-		"google/api/http.proto",
-		"google/api/resource.proto",
-		"google/longrunning/operations.proto",
-		"google/protobuf/any.proto",
-		"google/protobuf/descriptor.proto",
-		"google/protobuf/duration.proto",
-		"google/protobuf/empty.proto",
-		"google/protobuf/timestamp.proto",
-		"google/rpc/status.proto",
-	)
-	if err != nil {
-		panic(err)
-	}
-	commonProtos = cp
 }
