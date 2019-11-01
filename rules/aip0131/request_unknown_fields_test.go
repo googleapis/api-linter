736b531cd139fb28e0bbd162af6c3abc60cb8de2
Luke Sneeringer <lukesneeringer@google.com>
2019-11-01 11:03:49 -0700

    [AIP-131] Document message rules.

--- rules/aip0131/request_unknown_fields_test.go
+++ rules/aip0131/request_unknown_fields_test.go
@@ -23,39 +23,6 @@ import (
 	fpb "google.golang.org/genproto/protobuf/field_mask"
 )
 
-func TestStandardFields(t *testing.T) {
-	// Set up the testing permutations.
-	tests := []struct {
-		testName      string
-		messageName   string
-		nameFieldName string
-		problems      testutils.Problems
-	}{
-		{"Valid", "GetBookRequest", "name", testutils.Problems{}},
-		{"InvalidName", "GetBookRequest", "id", testutils.Problems{{Message: "name"}}},
-		{"Irrelevant", "AcquireBookRequest", "id", testutils.Problems{}},
-	}
-
-	// Run each test individually.
-	for _, test := range tests {
-		t.Run(test.testName, func(t *testing.T) {
-			// Create an appropriate message descriptor.
-			message, err := builder.NewMessage(test.messageName).AddField(
-				builder.NewField(test.nameFieldName, builder.FieldTypeString()),
-			).Build()
-			if err != nil {
-				t.Fatalf("Could not build %s message.", test.messageName)
-			}
-
-			// Run the lint rule, and establish that it returns the correct problems.
-			problems := standardFields.Lint(message.GetFile())
-			if diff := test.problems.SetDescriptor(message).Diff(problems); diff != "" {
-				t.Errorf("Problems did not match: %v", diff)
-			}
-		})
-	}
-}
-
 func TestStandardFieldsInvalidType(t *testing.T) {
 	// Create an appropriate message descriptor.
 	message, err := builder.NewMessage("GetBookRequest").AddField(
