5fcce87d2aa74307dca1b12094e73ec4dac843b2
Mingzhi Lin <mingzhi9@gmail.com>
2019-11-12 12:54:41 -0500

    Remove rule aliases specified to Google.

--- lint/rule_aliases.go
+++ lint/rule_aliases.go
@@ -1,11 +1,15 @@
 package lint
 
 // aliasMap stores legacy names for some rules.
-var aliasMap = map[string]string{
-	"core::0140::lower-snake":                  "naming-format",
-	"core::0140::enum-names::abbreviations":    "abbreviations",
-	"core::0140::field-names::abbreviations":   "abbreviations",
-	"core::0140::message-names::abbreviations": "abbreviations",
-	"core::0140::method-names::abbreviations":  "abbreviations",
-	"core::0140::service-names::abbreviations": "abbreviations",
-}
+// At Google, we inject rule-alias mapping into this map.
+// Example:
+// We will compile an addition file -- "google_rule_aliases.go".
+// ````````````````````````````````````````````````````````````
+// package lint
+//
+// func init() {
+// 	aliasMap["core::0140::lower-snake"] = "naming-format"
+// 	aliasMap["core::0140::enum-names::abbreviations"] = "abbreviations"
+// }
+// ````````````````````````````````````````````````````````````
+var aliasMap = map[string]string{}
