9be9cb588a6bb28d0d370351a174aeda9df942bf
weiqiw <michaelwqwq@gmail.com>
2019-11-12 11:54:13 -0500

    Merge branch 'master' into corp-rules2
    # Changes:
    #	modify aip0234.go and its related test

--- rules/aip0234/aip0234_test.go
+++ rules/aip0234/aip0234_test.go
@@ -15,18 +15,13 @@
 package aip0234
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/googleapis/api-linter/lint"
 )
 
 func TestAddRules(t *testing.T) {
-	rules := make(lint.RuleRegistry)
-	AddRules(rules)
-	for ruleName := range rules {
-		if !strings.HasPrefix(string(ruleName), "core::0234") {
-			t.Errorf("Rule %s is not namespaced to core::0234.", ruleName)
-		}
+	if err := AddRules(lint.NewRuleRegistry()); err != nil {
+		t.Errorf("AddRules got an error: %v", err)
 	}
 }
