d08a5fa2817e6bdbb3ccb535fff7fd22f28ce269
michael wang <michaelwqwq@gmail.com>
2019-11-18 20:40:55 -0500

    [AIP-234] Implement Batch Update rules. (#312)

---
+++ docs/rules/0234/http-body.md
@@ -0,0 +1,66 @@
+---
+rule:
+  aip: 234
+  name: [core, '0234', http-body]
+  summary: Batch Update methods should use `*` as the HTTP body.
+permalink: /234/http-body
+redirect_from:
+  - /0234/http-body
+---
+
+# Batch Update methods: HTTP body 
+
+This rule enforces that all `BatchUpdate` RPCs use `*` as the HTTP `body`, as 
+mandated in [AIP-234][].
+
+## Details
+
+This rule looks at any RPC methods matching beginning with `BatchUpdate`, and
+complains if HTTP `body` field is anything other than `*`.
+
+## Examples
+
+**Incorrect** code for this rule:
+
+```proto
+// Incorrect.
+rpc BatchUpdateBooks(BatchUpdateBooksRequest) returns (BatchUpdateBooksResponse) {
+  option (google.api.http) = {
+    post: "/v1/{parent=publishers/*}/books:batchUpdate"
+    body: "book"  // The http body should be "*".
+  };
+}
+```
+
+**Correct** code for this rule:
+
+```proto
+// Correct.
+rpc BatchUpdateBooks(BatchUpdateBooksRequest) returns (BatchUpdateBooksResponse) {
+  option (google.api.http) = {
+    post: "/v1/{parent=publishers/*}/books:batchUpdate"
+    body: "*"
+  };
+}
+```
+
+## Disabling
+
+If you need to violate this rule, use a leading comment above the method.
+Remember to also include an [aip.dev/not-precedent][] comment explaining why.
+
+```proto
+// (-- api-linter: core::0234::http-body=disabled
+//     aip.dev/not-precedent: We need to do this because reasons. --)
+rpc BatchUpdateBooks(BatchUpdateBooksRequest) returns (BatchUpdateBooksResponse) {
+  option (google.api.http) = {
+    post: "/v1/{parent=publishers/*}/books:batchUpdate"
+  };
+}
+```
+
+If you need to violate this rule for an entire file, place the comment at the
+top of the file.
+
+[aip-234]: https://aip.dev/234
+[aip.dev/not-precedent]: https://aip.dev/not-precedent
---
+++ rules/aip0234/aip0234.go
@@ -0,0 +1,55 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package aip0234 contains rules defined in https://aip.dev/234.
+package aip0234
+
+import (
+	"regexp"
+
+	"github.com/googleapis/api-linter/lint"
+	"github.com/jhump/protoreflect/desc"
+)
+
+// AddRules accepts a register function and registers each of
+// this AIP's rules to it.
+func AddRules(r lint.RuleRegistry) error {
+	return r.Register(
+		234,
+		httpBody,
+		httpMethod,
+		httpUriSuffix,
+	)
+}
+
+var batchUpdateMethodRegexp = regexp.MustCompile("^BatchUpdate(?:[A-Za-z0-9]|$)")
+var batchUpdateReqMessageRegexp = regexp.MustCompile("^BatchUpdate[A-Za-z0-9]*Request$")
+var batchUpdateResMessageRegexp = regexp.MustCompile("^BatchUpdate[A-Za-z0-9]*Response$")
+var batchUpdateURINameRegexp = regexp.MustCompile("\\{[a-zA-Z=/*]+\\}\\/[A-Za-z0-9-=/*]+:batchUpdate$")
+var updateReqMessageRegexp = regexp.MustCompile("^Update[A-Za-z0-9]*Request$")
+
+// Returns true if this is a AIP-234 Batch Update method, false otherwise.
+func isBatchUpdateMethod(m *desc.MethodDescriptor) bool {
+	return batchUpdateMethodRegexp.MatchString(m.GetName())
+}
+
+// Returns true if this is an AIP-234 Batch Update request message, false otherwise.
+func isBatchUpdateRequestMessage(m *desc.MessageDescriptor) bool {
+	return batchUpdateReqMessageRegexp.MatchString(m.GetName())
+}
+
+// Returns true if this is an AIP-234 Batch Update request message, false otherwise.
+func isBatchUpdateResponseMessage(m *desc.MessageDescriptor) bool {
+	return batchUpdateResMessageRegexp.MatchString(m.GetName())
+}
