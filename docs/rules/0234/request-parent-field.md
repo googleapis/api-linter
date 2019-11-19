f04c9ee46d22299e8b7fc463e38a4c5c0406bcfc
michael wang <michaelwqwq@gmail.com>
2019-11-19 11:44:37 -0500

    [AIP-234] More batch update rules (#359)

---
+++ docs/rules/0234/request-parent-field.md
@@ -0,0 +1,71 @@
+---
+rule:
+  aip: 234
+  name: [core, '0234', request-parent-field]
+  summary: Batch Update RPCs must have a `parent` field in the request.
+permalink: /234/request-parent-field
+redirect_from:
+  - /0234/request-parent-field
+---
+
+# Batch Update methods: Parent field
+
+This rule enforces that all `BatchUpdate` methods have a `string parent` field
+in the request message, as mandated in [AIP-234][].
+
+## Details
+
+This rule looks at any message matching `BatchUpdate*Request` and complains if
+either the `parent` field is missing, or if it has any type other than
+`string`.
+
+## Examples
+
+**Incorrect** code for this rule:
+
+```proto
+// Incorrect.
+message BatchUpdateBooksRequest {
+  string publisher = 1;  // Field name should be `parent`.
+  repeated UpdateBookRequest requests = 2;
+}
+```
+
+```proto
+// Incorrect.
+message BatchUpdateBookRequest {
+  bytes parent = 1;  // Field type should be `string`.
+  repeated UpdateBookRequest requests = 2;
+}
+```
+
+**Correct** code for this rule:
+
+```proto
+// Correct.
+message BatchUpdateBookRequest {
+  string parent = 1;
+  repeated UpdateBookRequest requests = 2;
+}
+```
+
+## Disabling
+
+If you need to violate this rule, use a leading comment above the message (if
+the `parent` field is missing) or above the field (if it is the wrong type).
+Remember to also include an [aip.dev/not-precedent][] comment explaining why.
+
+```proto
+// (-- api-linter: core::0234::request-parent-field=disabled
+//     aip.dev/not-precedent: We need to do this because reasons. --)
+message BatchUpdateBooksRequest {
+  string publisher = 1;  // Field name should be `parent`.
+  repeated UpdateBookRequest requests = 2;
+}
+```
+
+If you need to violate this rule for an entire file, place the comment at the
+top of the file.
+
+[aip-234]: https://aip.dev/234
+[aip.dev/not-precedent]: https://aip.dev/not-precedent
---
+++ rules/aip0234/response_resource_field_test.go
@@ -0,0 +1,76 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0234
+
+import (
+	"testing"
+
+	"github.com/googleapis/api-linter/rules/internal/testutils"
+	"github.com/jhump/protoreflect/desc"
+)
+
+func TestResponseResourceField(t *testing.T) {
+	// Set up the testing permutations.
+	tests := []struct {
+		testName    string
+		Field       string
+		problems    testutils.Problems
+		problemDesc func(m *desc.MessageDescriptor) desc.Descriptor
+	}{
+		{
+			testName: "Valid",
+			Field:    "repeated Book books",
+			problems: testutils.Problems{},
+		},
+		{
+			testName: "FieldIsNotRepeated",
+			Field:    "Book book",
+			problems: testutils.Problems{{Message: "repeated"}},
+		},
+		{
+			testName: "MissingField",
+			Field:    "string response",
+			problems: testutils.Problems{{Message: "no \"Book\" type field"}},
+			problemDesc: func(m *desc.MessageDescriptor) desc.Descriptor {
+				return m
+			},
+		},
+	}
+
+	// Run each test individually.
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			file := testutils.ParseProto3Tmpl(t, `
+				message BatchUpdateBooksResponse {
+					{{.Field}} = 1;
+				}
+				message Book {
+				}`, test)
+
+			m := file.GetMessageTypes()[0]
+
+			// Determine the descriptor that a failing test will attach to.
+			var problemDesc desc.Descriptor = m.GetFields()[0]
+			if test.problemDesc != nil {
+				problemDesc = test.problemDesc(m)
+			}
+
+			problems := responseResourceField.Lint(file)
+			if diff := test.problems.SetDescriptor(problemDesc).Diff(problems); diff != "" {
+				t.Errorf(diff)
+			}
+		})
+	}
+}
