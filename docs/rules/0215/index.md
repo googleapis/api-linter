d469027dd94dc831f0a1f108e0136dd13d52e400
Luke Sneeringer <lukesneeringer@google.com>
2019-12-05 11:15:38 -0800

    [AIP-215] Proto packages should end in a version. (#413)

---
+++ docs/rules/0215/index.md
@@ -0,0 +1,10 @@
+---
+aip_listing: 215
+permalink: /215/
+redirect_from:
+  - /0215/
+---
+
+# Common component versions
+
+{% include linter-aip-listing.md aip=215 %}
---
+++ rules/aip0215/versioned_packages_test.go
@@ -0,0 +1,57 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0215
+
+import (
+	"testing"
+
+	"github.com/googleapis/api-linter/rules/internal/testutils"
+)
+
+func TestVersionedPackages(t *testing.T) {
+	for _, test := range []struct {
+		name        string
+		PackageStmt string
+		problems    testutils.Problems
+	}{
+		{"Stable", "package foo.bar.v1;", testutils.Problems{}},
+		{"StableBignum", "package foo.bar.v999;", testutils.Problems{}},
+		{"Alpha", "package foo.bar.v1alpha;", testutils.Problems{}},
+		{"Alpha1", "package foo.bar.v1alpha1;", testutils.Problems{}},
+		{"Alpha12", "package foo.bar.v1alpha12;", testutils.Problems{}},
+		{"Beta", "package foo.bar.v1beta;", testutils.Problems{}},
+		{"Beta1", "package foo.bar.v1beta1;", testutils.Problems{}},
+		{"Beta12", "package foo.bar.v1beta12;", testutils.Problems{}},
+		{"P1Beta1", "package foo.bar.v1p1beta1;", testutils.Problems{}},
+		{"P12Beta1", "package foo.bar.v1p12beta1;", testutils.Problems{}},
+		{"P1Beta12", "package foo.bar.v1p1beta12;", testutils.Problems{}},
+		{"P12Beta12", "package foo.bar.v1p12beta12;", testutils.Problems{}},
+		{"EAP", "package foo.bar.v1eap;", testutils.Problems{}},
+		{"EAP1", "package foo.bar.v1eap1;", testutils.Problems{}},
+		{"Test", "package foo.bar.v1test;", testutils.Problems{}},
+		{"Test1", "package foo.bar.v1test1;", testutils.Problems{}},
+		{"Type", "package foo.bar.type;", testutils.Problems{}},
+		{"InvalidNoVersion", "package foo.bar;", testutils.Problems{{Message: "versioned packages"}}},
+		{"InvalidSubpackage", "package foo.bar.v1.resources;", testutils.Problems{{Message: "versioned packages"}}},
+		{"IgnoredNoPackage", "", testutils.Problems{}},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			f := testutils.ParseProto3Tmpl(t, "{{.PackageStmt}}", test)
+			if diff := test.problems.SetDescriptor(f).Diff(versionedPackages.Lint(f)); diff != "" {
+				t.Errorf(diff)
+			}
+		})
+	}
+}
