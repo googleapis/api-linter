c75ff57205feaf80d51a96b28e883c7579139d7a
Luke Sneeringer <lukesneeringer@google.com>
2019-12-09 17:39:36 -0800

    [AIP-134] Enforce a consistent method signature. (#435)

---
+++ rules/aip0134/method_signature.go
@@ -0,0 +1,67 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0134
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/googleapis/api-linter/lint"
+	"github.com/googleapis/api-linter/locations"
+	"github.com/googleapis/api-linter/rules/internal/utils"
+	"github.com/jhump/protoreflect/desc"
+	"github.com/stoewer/go-strcase"
+)
+
+var methodSignature = &lint.MethodRule{
+	Name:   lint.NewRuleName(134, "method-signature"),
+	OnlyIf: isUpdateMethod,
+	LintMethod: func(m *desc.MethodDescriptor) []lint.Problem {
+		signatures := utils.GetMethodSignatures(m)
+		want := []string{
+			strcase.SnakeCase(strings.TrimPrefix(m.GetName(), "Update")),
+			"update_mask",
+		}
+
+		// Check if the signature is missing.
+		if len(signatures) == 0 {
+			return []lint.Problem{{
+				Message: fmt.Sprintf(
+					"Update methods should include `(google.api.method_signature) = %q`",
+					strings.Join(want, ","),
+				),
+				Descriptor: m,
+			}}
+		}
+
+		// Check if the signature is wrong.
+		if !reflect.DeepEqual(signatures[0], want) {
+			return []lint.Problem{{
+				Message: fmt.Sprintf(
+					"The method signature for Update methods should be %q.",
+					strings.Join(want, ","),
+				),
+				Suggestion: fmt.Sprintf(
+					"option (google.api.method_signature) = %q;",
+					strings.Join(want, ","),
+				),
+				Descriptor: m,
+				Location:   locations.MethodSignature(m, 0),
+			}}
+		}
+		return nil
+	},
+}
