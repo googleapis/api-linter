2ccc8c4857d67df1716d3dbe979582be7fbd0718
Luke Sneeringer <lukesneeringer@google.com>
2020-02-24 17:47:52 -0800

    [AIP-192] Add rule for trademarked names. (#495)

---
+++ docs/rules/0192/trademarked-names.md
@@ -0,0 +1,88 @@
+---
+rule:
+  aip: 192
+  name: [core, '0192', trademarked-names]
+  summary: Trademarked names should be used correctly.
+permalink: /192/trademarked-names
+redirect_from:
+  - /0192/trademarked-names
+---
+
+# Trademarked names
+
+This rule enforces trademarked names in public comments are not abbreviated and
+follow the trademark owner's branding style, as mandated in [AIP-192][].
+
+## Details
+
+This rule looks at each descriptor in each proto file (exempting oneofs and the
+file itself) and complains if it catches a common trademark mistake. It only
+checks against a known problem list, and does not make guesses beyond that.
+
+It currently catches common mistaken variants for:
+
+- App Engine
+- BigQuery
+- Bigtable
+- Bitbucket
+- Cloud Storage
+- Compute Engine
+- Dataflow
+- Dataprep
+- Dialogflow
+- Directory Sync
+- GitHub
+- GitLab
+- G Suite
+- Pub/Sub
+- Service Mesh
+- Stack Overflow
+
+## Examples
+
+**Incorrect** code for this rule:
+
+```proto
+// Incorrect.
+message Book {
+  string name = 1;
+
+  // A repository containing Markdown files for each chapter of
+  // the book on Github.
+  // (--           ^ Should be GitHub. --)
+  string github_repo = 2;
+}
+```
+
+**Correct** code for this rule:
+
+```proto
+// Correct.
+message Book {
+  string name = 1;
+
+  // A repository containing Markdown files for each chapter of
+  // the book on GitHub.
+  string github_repo = 2;
+}
+```
+
+## Disabling
+
+If you need to violate this rule, use a leading comment above the descriptor.
+Remember to also include an [aip.dev/not-precedent][] comment explaining why.
+
+```proto
+message Book {
+  string name = 1;
+
+  // (-- api-linter: core::0192::trademarked-names=disabled
+  //     aip.dev/not-precedent: We need to do this because reasons. --)
+  // A repository containing Markdown files for each chapter of
+  // the book on Github.
+  string github_repo = 2;
+}
+```
+
+[aip-192]: https://aip.dev/192
+[aip.dev/not-precedent]: https://aip.dev/not-precedent
---
+++ rules/aip0192/trademarked_names.go
@@ -0,0 +1,82 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0192
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/googleapis/api-linter/lint"
+	"github.com/jhump/protoreflect/desc"
+)
+
+var trademarkAliases = map[string][]string{
+	"App Engine":     []string{"GAE", "gae", "AppEngine", "App engine"},
+	"BigQuery":       []string{"Bigquery", "Big Query", "BQ"},
+	"BigQuery ML":    []string{"BQML", "bqml"},
+	"Bigtable":       []string{"BigTable", "Big Table", "Big table"},
+	"Bitbucket":      []string{"BitBucket", "Bit Bucket"},
+	"Cloud Storage":  []string{"GCS", "gcs"},
+	"Compute Engine": []string{"GCE", "gce"},
+	"Dataflow":       []string{"Data Flow", "Data flow", "DataFlow"},
+	"Dataprep":       []string{"Data Prep", "Data prep", "DataPrep"},
+	"Dialogflow":     []string{"DialogFlow", "Dialog Flow", "Dialog flow"},
+	"Directory Sync": []string{"GCDS", "CDS", "gcds", "cds", "DirectorySync"},
+	"GitHub":         []string{"Github", "Git Hub"},
+	"GitLab":         []string{"Gitlab", "Git Lab"},
+	"G Suite":        []string{"GSuite", "G-Suite", "gSuite"},
+	"Pub/Sub":        []string{"PubSub", "Pubsub", "Cloud Pub/Sub"},
+	"Service Mesh":   []string{"ASM", "CSM", "GCSM", "csm", "gcsm"},
+	"Stack Overflow": []string{"StackOverflow"},
+}
+
+// We actually want regexes so we do not accidentally false-positive acronyms
+// that *contain* our matches. (For example, "BQD" should not match and tell us
+// to change to BigQuery.)
+func defaultTrademarkTypos() map[string][]*regexp.Regexp {
+	tmRegexes := map[string][]*regexp.Regexp{}
+	for k, tms := range trademarkAliases {
+		tmReg := []*regexp.Regexp{}
+		for _, tm := range tms {
+			tmReg = append(tmReg, regexp.MustCompile(`\b`+strings.ReplaceAll(tm, " ", `\s+`)+`\b`))
+		}
+		tmRegexes[k] = tmReg
+	}
+	return tmRegexes
+}
+
+var tmRegexes = defaultTrademarkTypos()
+
+var trademarkedNames = &lint.DescriptorRule{
+	Name: lint.NewRuleName(192, "trademarked-names"),
+	LintDescriptor: func(d desc.Descriptor) (problems []lint.Problem) {
+		c := strings.Join(
+			separateInternalComments(d.GetSourceInfo().GetLeadingComments()).External,
+			"\n",
+		)
+		for want, badThings := range tmRegexes {
+			for _, bad := range badThings {
+				if bad.MatchString(c) {
+					problems = append(problems, lint.Problem{
+						Message:    fmt.Sprintf("Use %q in comments, not %q.", want, bad),
+						Descriptor: d,
+					})
+				}
+			}
+		}
+		return
+	},
+}
