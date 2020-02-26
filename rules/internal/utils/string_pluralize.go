5df353c95d09f1ccc797266efa2e139301cae337
Supicha Phadungsilp <supichapd@gmail.com>
2020-02-26 02:02:52 -0800

    [AIP-158] Response's first field must be plural (#496)
    * [AIP-158] response's first field must be plural
    
    * Remove unused code
    
    * Add comment and exclude case that messages don't contain next_page_token field
    
    * update test case
    
    * Add metadata file and change from `must be` to `should be`
    
    * update metadata summary
    
    * Change rule name to `response-plural-first-field` and get first field by position
    
    * run prettier on response-plural-first-field.md
    
    * [AIP-158] change from `studentProfiles` to `student_profiles`
    
    * [fix] code style
    
    * [fix] pluralization
    
    * Create `toPlural` function and update isPaginationMessage definition
    
    * add test case for pluralize
    
    * move ToPlural to rules/internal/utils/string_pluralize.go
    
    Co-authored-by: Mingzhi Lin <mingzhi@users.noreply.github.com>
    Co-authored-by: Luke Sneeringer <luke@sneeringer.com>

---
+++ rules/internal/utils/string_pluralize.go
@@ -0,0 +1,28 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 		https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package utils
+
+import (
+	"github.com/gertd/go-pluralize"
+)
+
+var pluralizeClient = pluralize.NewClient()
+
+func ToPlural(s string) string {
+	// Need to convert name to singular first to support none standard case such as persons, cactuses.
+	// persons -> person -> people
+
+	return pluralizeClient.Plural(pluralizeClient.Singular(s))
+}
