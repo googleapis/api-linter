09b6d31a12aab1569ecb3728e47f7d8c91f5145d
Luke Sneeringer <lukesneeringer@google.com>
2020-06-15 10:42:04 -0700

    [AIP-216] Add lint rule for common state value synonyms.
    This rule catches common State mistakes like `SUCCESSFUL` and
    `SUCCESS` and corrects them.
    
    It also catches `CANCELED` and asks for the two-L spelling:
    `CANCELLED`. Both are correct, but we should use one consistently
    and the AIP uses the two-L version.

---
+++ rules/aip0216/value_synonyms.go
@@ -0,0 +1,54 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0216
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/googleapis/api-linter/lint"
+	"github.com/googleapis/api-linter/locations"
+	"github.com/jhump/protoreflect/desc"
+)
+
+var valueSynonyms = &lint.EnumValueRule{
+	Name: lint.NewRuleName(216, "value-synonyms"),
+	OnlyIf: func(v *desc.EnumValueDescriptor) bool {
+		if strings.HasSuffix(v.GetEnum().GetName(), "State") {
+			return true
+		}
+		return false
+	},
+	LintEnumValue: func(v *desc.EnumValueDescriptor) []lint.Problem {
+		for bad, good := range map[string]string{
+			"CANCELED":   "CANCELLED",
+			"FAIL":       "FAILED",
+			"FAILURE":    "FAILED",
+			"READY":      "ACTIVE",
+			"SUCCESS":    "SUCCEEDED",
+			"SUCCESSFUL": "SUCCEEDED",
+		} {
+			if v.GetName() == bad {
+				return []lint.Problem{{
+					Message:    fmt.Sprintf("Prefer %q over %q for state names.", good, bad),
+					Suggestion: good,
+					Descriptor: v,
+					Location:   locations.DescriptorName(v),
+				}}
+			}
+		}
+		return nil
+	},
+}
---
+++ rules/aip0216/value_synonyms_test.go
@@ -0,0 +1,52 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0216
+
+import (
+	"testing"
+
+	"github.com/googleapis/api-linter/rules/internal/testutils"
+)
+
+func TestValueSynonyms(t *testing.T) {
+	for _, test := range []struct {
+		name      string
+		EnumName  string
+		ValueName string
+		problems  testutils.Problems
+	}{
+		{"ValidSucceeded", "State", "SUCCEEDED", nil},
+		{"ValidDeleted", "State", "DELETED", nil},
+		{"InvalidSuccess", "State", "SUCCESS", testutils.Problems{{Suggestion: "SUCCEEDED"}}},
+		{"InvalidSuccessful", "State", "SUCCESSFUL", testutils.Problems{{Suggestion: "SUCCEEDED"}}},
+		{"InvalidCanceled", "State", "CANCELED", testutils.Problems{{Suggestion: "CANCELLED"}}},
+		{"InvalidFail", "State", "FAIL", testutils.Problems{{Suggestion: "FAILED"}}},
+		{"InvalidFailure", "State", "FAILURE", testutils.Problems{{Suggestion: "FAILED"}}},
+		{"Ireelevant", "Foo", "SUCCESSFUL", nil},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			file := testutils.ParseProto3Tmpl(t, `
+				enum {{.EnumName}} {
+					{{.EnumName}}_UNSPECIFIED = 0;
+					{{.ValueName}} = 1;
+				}
+			`, test)
+			ev := file.GetEnumTypes()[0].GetValues()[1]
+			if diff := test.problems.SetDescriptor(ev).Diff(valueSynonyms.Lint(file)); diff != "" {
+				t.Errorf(diff)
+			}
+		})
+	}
+}
