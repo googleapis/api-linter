0ec04e5c9cc6b54333e8bfcf6d0b029b84f7b9d8
Kimberly <kmilam@google.com>
2020-06-26 19:11:14 -0400

    Revert refactoring

--- rules/aip0142/time_field_names.go
+++ rules/aip0142/time_field_names.go
@@ -46,21 +46,19 @@ var fieldNames = &lint.FieldRule{
 
 		// Look for timestamps that do not end in `_time` or, if repeated, `_times`.
 		if utils.GetTypeName(f) == "google.protobuf.Timestamp" && !strings.HasSuffix(f.GetName(), "_time") {
-			if f.IsRepeated() {
-				if strings.HasSuffix(f.GetName(), "_times") {
-					return nil
-				}
+			if !f.IsRepeated() {
+				return []lint.Problem{{
+					Message:    "Timestamp fields should end in `_time`.",
+					Descriptor: f,
+					Location:   locations.DescriptorName(f),
+				}}
+			} else if !strings.HasSuffix(f.GetName(), "_times") {
 				return []lint.Problem{{
 					Message:    "Repeated Timestamp fields should end in `_time` or `_times`.",
 					Descriptor: f,
 					Location:   locations.DescriptorName(f),
 				}}
 			}
-			return []lint.Problem{{
-				Message:    "Timestamp fields should end in `_time`.",
-				Descriptor: f,
-				Location:   locations.DescriptorName(f),
-			}}
 		}
 
 		return nil
