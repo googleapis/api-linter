c50d6c1318e14b0a27ef8cb77137e0d9ad680272
Andrew Paseltiner <apaseltiner@google.com>
2020-10-05 17:40:12 -0400

    feat: Add rules for batch-method requests field behavior (#644)

--- rules/aip0234/aip0234.go
+++ rules/aip0234/aip0234.go
@@ -33,6 +33,7 @@ func AddRules(r lint.RuleRegistry) error {
 		pluralMethodName,
 		requestMessageName,
 		requestParentField,
+		requestRequestsBehavior,
 		requestRequestsField,
 		responseMessageName,
 		responseResourceField,
