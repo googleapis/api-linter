0179fe6b4fc062125d73d2d8172113b5486d7deb
Andrew Paseltiner <apaseltiner@google.com>
2020-10-14 12:26:30 -0400

    Add rule for AIP-128 annotations field

---
+++ rules/aip0128/aip0128.go
@@ -0,0 +1,29 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package aip0128 contains rules defined in https://aip.dev/128.
+package aip0128
+
+import (
+	"github.com/googleapis/api-linter/lint"
+)
+
+// AddRules accepts a register function and registers each of
+// this AIP's rules to it.
+func AddRules(r lint.RuleRegistry) error {
+	return r.Register(
+		128,
+		resourceAnnotationsField,
+	)
+}
