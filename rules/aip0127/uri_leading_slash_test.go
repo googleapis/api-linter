21efc22f2240a15659ee5d8087f21ff1df34c4e0
Luke Sneeringer <lukesneeringer@google.com>
2021-01-29 09:26:59 -0800

    feat: Require URIs to begin with a forward slash. (#738)

---
+++ rules/aip0127/uri_leading_slash_test.go
@@ -0,0 +1,50 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0127
+
+import (
+	"testing"
+
+	"github.com/googleapis/api-linter/rules/internal/testutils"
+)
+
+func TestURILeadingSlash(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		URI      string
+		problems testutils.Problems
+	}{
+		{"Valid", "/v1/{name=publishers/*/books/*}", nil},
+		{"Invalid", "v1/{name=publishers/*/books/*}", testutils.Problems{{Message: "leading slash"}}},
+	} {
+		f := testutils.ParseProto3Tmpl(t, `
+			import "google/api/annotations.proto";
+			import "google/protobuf/empty.proto";
+
+			service Foo {
+				rpc FrobFoo(google.protobuf.Empty) returns (google.protobuf.Empty) {
+					option (google.api.http) = {
+						post: "{{.URI}}"
+						body: "*"
+					};
+				}
+			}
+		`, test)
+		m := f.GetServices()[0].GetMethods()[0]
+		if diff := test.problems.SetDescriptor(m).Diff(leadingSlash.Lint(f)); diff != "" {
+			t.Errorf(diff)
+		}
+	}
+}
