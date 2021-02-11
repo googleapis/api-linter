2a6b5d4b42168d8d4c153a2cd83ed4c8c414da2b
Andrew Paseltiner <apaseltiner@google.com>
2021-02-11 12:19:17 -0500

    feat: Add rule for AIP-162 Delete Revision HTTP URI suffix (#755)

---
+++ rules/aip0162/delete_revision_http_uri_suffix.go
@@ -0,0 +1,39 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0162
+
+import (
+	"github.com/googleapis/api-linter/lint"
+	"github.com/googleapis/api-linter/rules/internal/utils"
+	"github.com/jhump/protoreflect/desc"
+)
+
+// Delete Revision methods should have a proper HTTP pattern.
+var deleteRevisionHTTPURISuffix = &lint.MethodRule{
+	Name:   lint.NewRuleName(162, "delete-revision-http-uri-suffix"),
+	OnlyIf: isDeleteRevisionMethod,
+	LintMethod: func(m *desc.MethodDescriptor) []lint.Problem {
+		for _, httpRule := range utils.GetHTTPRules(m) {
+			if !deleteRevisionURINameRegexp.MatchString(httpRule.URI) {
+				return []lint.Problem{{
+					Message:    `Delete Revision URI should end with ":deleteRevision".`,
+					Descriptor: m,
+				}}
+			}
+		}
+
+		return nil
+	},
+}
