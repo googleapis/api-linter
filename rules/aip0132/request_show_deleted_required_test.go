b89e1df7a19a1ba87f455d02c070e28b34df89bb
Andrew Paseltiner <apaseltiner@google.com>
2021-04-07 14:30:09 -0400

    Require show_deleted List request field for resources supporting soft delete

---
+++ rules/aip0132/request_show_deleted_required_test.go
@@ -0,0 +1,44 @@
+package aip0132
+
+import (
+	"testing"
+
+	"github.com/googleapis/api-linter/rules/internal/testutils"
+)
+
+func TestRequestShowDeletedRequired(t *testing.T) {
+	tests := []struct {
+		name     string
+		Message  string
+		Method   string
+		Field    string
+		problems testutils.Problems
+	}{
+		{"Valid", "ListBooksRequest", "UndeleteBook", `bool show_deleted = 1;`, nil},
+		{"Invalid", "ListBooksRequest", "UndeleteBook", ``, testutils.Problems{{Message: "show_deleted"}}},
+		{"IrrelevantNoSoftDelete", "ListBooksRequest", "GetBook", ``, nil},
+		{"IrrelevantMessage", "EnumerateBooksRequest", "UndeleteBook", "", nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			f := testutils.ParseProto3Tmpl(t, `
+				service Library {
+					rpc {{.Method}}({{.Method}}Request) returns (Book);
+				}
+				message Book {}
+				message {{.Message}} {
+					{{.Field}}
+				}
+				message ListBooksResponse {
+					repeated Book books = 1;
+				}
+				message {{.Method}}Request {}
+			`, test)
+			problems := requestShowDeletedRequired.Lint(f)
+			if diff := test.problems.SetDescriptor(f.GetMessageTypes()[1]).Diff(problems); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
