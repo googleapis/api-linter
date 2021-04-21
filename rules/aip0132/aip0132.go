199432d50c952ba22b832a93bc5724b64bc4ed17
Andrew Paseltiner <apaseltiner@google.com>
2021-04-21 15:59:38 -0400

    feat: Require show_deleted List request field for resources supporting soft delete (#833)

--- rules/aip0132/aip0132.go
+++ rules/aip0132/aip0132.go
@@ -38,6 +38,7 @@ func AddRules(r lint.RuleRegistry) error {
 		requestParentReference,
 		requestParentValidReference,
 		requestParentRequired,
+		requestShowDeletedRequired,
 		responseMessageName,
 		responseUnknownFields,
 		unknownFields,
---
+++ rules/aip0132/request_show_deleted_required.go
@@ -0,0 +1,41 @@
+package aip0132
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/googleapis/api-linter/lint"
+	"github.com/googleapis/api-linter/rules/internal/utils"
+	"github.com/jhump/protoreflect/desc"
+	"github.com/stoewer/go-strcase"
+)
+
+// List requests should contain a show_deleted field if the resource supports
+// soft delete.
+var requestShowDeletedRequired = &lint.MessageRule{
+	Name: lint.NewRuleName(132, "request-show-deleted-required"),
+	OnlyIf: func(m *desc.MessageDescriptor) bool {
+		if !isListRequestMessage(m) {
+			return false
+		}
+		// Check for soft-delete support by getting the resource name
+		// from the corresponding response message.
+		plural := strings.TrimPrefix(strings.TrimSuffix(m.GetName(), "Request"), "List")
+		if resp := utils.FindMessage(m.GetFile(), fmt.Sprintf("List%sResponse", plural)); resp != nil {
+			if paged := resp.FindFieldByName(strcase.SnakeCase(plural)); paged != nil {
+				singular := paged.GetMessageType().GetName()
+				return utils.FindMethod(m.GetFile(), "Undelete"+singular) != nil
+			}
+		}
+		return false
+	},
+	LintMessage: func(m *desc.MessageDescriptor) []lint.Problem {
+		if m.FindFieldByName("show_deleted") != nil {
+			return nil
+		}
+		return []lint.Problem{{
+			Message:    "List requests for resources supporting soft delete must have a `bool show_deleted` field.",
+			Descriptor: m,
+		}}
+	},
+}
