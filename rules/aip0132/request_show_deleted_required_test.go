16cdf41316a41ece9049ae06074606fd5c3f063b
Andrew Paseltiner <apaseltiner@google.com>
2021-07-02 14:31:45 -0400

    fix: Prevent AIP-132 request-show-deleted-required rule from crashing on non-message fields (#856)
    Fixes #854

--- rules/aip0132/request_show_deleted_required_test.go
+++ rules/aip0132/request_show_deleted_required_test.go
@@ -42,3 +42,17 @@ func TestRequestShowDeletedRequired(t *testing.T) {
 		})
 	}
 }
+
+// Regression test for https://github.com/googleapis/api-linter/issues/854.
+func TestRequestShowDeletedRequired_NonMessageType(t *testing.T) {
+	f := testutils.ParseProto3Tmpl(t, `
+		message ListBooksRequest {}
+		message ListBooksResponse {
+			repeated string books = 1;
+		}
+	`, nil)
+	problems := requestShowDeletedRequired.Lint(f)
+	if diff := (testutils.Problems{}).SetDescriptor(f.GetMessageTypes()[0]).Diff(problems); diff != "" {
+		t.Error(diff)
+	}
+}
