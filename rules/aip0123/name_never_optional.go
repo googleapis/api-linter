7f9a47069e3e1eea9ad9aa189979723d867e86ec
Noah Dietz <noahdietz@users.noreply.github.com>
2022-09-07 15:51:23 -0700

    feat: name field must never be proto3_optional (#1011)

---
+++ rules/aip0123/name_never_optional.go
@@ -0,0 +1,51 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0123
+
+import (
+	"github.com/googleapis/api-linter/lint"
+	"github.com/googleapis/api-linter/locations"
+	"github.com/googleapis/api-linter/rules/internal/utils"
+	"github.com/jhump/protoreflect/desc"
+)
+
+var nameNeverOptional = &lint.MessageRule{
+	Name: lint.NewRuleName(123, "name-never-optional"),
+	OnlyIf: func(m *desc.MessageDescriptor) bool {
+		f := "name"
+		if nf := utils.GetResource(m).GetNameField(); nf != "" {
+			f = nf
+		}
+		return utils.IsResource(m) && m.FindFieldByName(f) != nil
+	},
+	LintMessage: func(m *desc.MessageDescriptor) []lint.Problem {
+		f := "name"
+		if nf := utils.GetResource(m).GetNameField(); nf != "" {
+			f = nf
+		}
+		field := m.FindFieldByName(f)
+
+		if field.IsProto3Optional() {
+			return []lint.Problem{{
+				Message:    "Resource name fields must never be labeled with proto3_optional",
+				Descriptor: field,
+				Location:   locations.FieldLabel(field),
+				Suggestion: "",
+			}}
+		}
+
+		return nil
+	},
+}
