b28d873401f987e556f21dbcb1cf7ae0bb2b4642
Anthony Amadeo <anthony.c.amadeo@gmail.com>
2022-12-05 21:14:03 +0000

    Add even more tests

--- rules/aip0127/http_template_syntax_test.go
+++ rules/aip0127/http_template_syntax_test.go
@@ -54,10 +54,11 @@ func TestHttpTemplateSyntax(t *testing.T) {
 		{"VariableWithDoubleWildcardFollowedByLiteral", "/v1/{field=**/books}", true},
 		{"VariableWithLiteralFollowedBySingleWildcard", "/v1/{field=books/*}", true},
 		{"VariableWithLiteralFollowedByDoubleWildcard", "/v1/{field=books/**}", true},
-		{"VariableFollowedByLiteralSegment", "/v1/{field}/books", true},
-		{"VariableFollowedByVariableSegment", "/v1/{field}/{otherField}", true},
-		{"VariableWithTemplateFollowedByLiteralSegment", "/v1/{field=books/*}/shelves", true},
-		{"VariableFollowedByVariableWithTemplateSegment", "/v1/{field}/{otherField=books/*}", true},
+		{"VariableFollowedByLiteral", "/v1/{field}/books", true},
+		{"VariableFollowedByVariable", "/v1/{field}/{otherField}", true},
+		{"VariableWithTemplateFollowedByLiteral", "/v1/{field=books/*}/shelves", true},
+		{"VariableFollowedByVariableWithTemplate", "/v1/{field}/{otherField=books/*}", true},
+		{"VariableWithTemplateFollowedByVariableWithTemplate", "/v1/{field=books/*}/{otherField=shelves/*}", true},
 
 		// Invalid cases
 		{"LiteralWithoutLeadingSlash", "v1", false},
