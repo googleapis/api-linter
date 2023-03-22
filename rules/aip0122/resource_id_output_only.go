6872311df1c1e3a9ebfabbaa647c58aa6a7172ac
Noah Dietz <noahdietz@users.noreply.github.com>
2023-03-22 15:50:16 -0700

    feat(AIP-122): enforce output_only resource IDs (#1125)
    * feat(AIP-122): enforce output_only resource IDs
    
    * only lint expected resource ID field

---
+++ rules/aip0122/resource_id_output_only.go
@@ -0,0 +1,60 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0122
+
+import (
+	"github.com/googleapis/api-linter/lint"
+	"github.com/googleapis/api-linter/rules/internal/utils"
+	"github.com/jhump/protoreflect/desc"
+	"github.com/stoewer/go-strcase"
+	"google.golang.org/genproto/googleapis/api/annotations"
+)
+
+var resourceIdOutputOnly = &lint.FieldRule{
+	Name: lint.NewRuleName(122, "resource-id-output-only"),
+	OnlyIf: func(f *desc.FieldDescriptor) bool {
+		var idName string
+		p := f.GetParent().(*desc.MessageDescriptor)
+
+		// Build an expected ID field name based on the Resource `singular`
+		// field or by parsing the `type`.
+		isRes := utils.IsResource(p)
+		if isRes {
+			res := utils.GetResource(p)
+			idName = res.GetSingular()
+			if idName == "" {
+				if _, t, ok := utils.SplitResourceTypeName(res.GetType()); ok {
+					idName = strcase.SnakeCase(t)
+				}
+			}
+			idName += "_id"
+		}
+
+		isId := f.GetName() == "uid" || f.GetName() == idName
+		return isRes && isId
+	},
+	LintField: func(f *desc.FieldDescriptor) []lint.Problem {
+		behaviors := utils.GetFieldBehavior(f)
+		if !behaviors.Contains(annotations.FieldBehavior_OUTPUT_ONLY.String()) {
+			return []lint.Problem{
+				{
+					Message:    "Resource ID fields must have field_behavior OUTPUT_ONLY",
+					Descriptor: f,
+				},
+			}
+		}
+		return nil
+	},
+}
