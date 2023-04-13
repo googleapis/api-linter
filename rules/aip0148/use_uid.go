b7a8a514473ccf5820710c44a82ed924c5abae08
noahdietz <ndietz@google.com>
2023-04-13 08:16:20 -0700

    feat(AIP-148): use uid instead of id

---
+++ rules/aip0148/use_uid.go
@@ -0,0 +1,44 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aip0148
+
+import (
+	"fmt"
+
+	"github.com/googleapis/api-linter/lint"
+	"github.com/googleapis/api-linter/locations"
+	"github.com/googleapis/api-linter/rules/internal/utils"
+	"github.com/jhump/protoreflect/desc"
+)
+
+var useUid = &lint.FieldRule{
+	Name: lint.NewRuleName(148, "use-uid"),
+	OnlyIf: func(f *desc.FieldDescriptor) bool {
+		return utils.IsResource(f.GetOwner())
+	},
+	LintField: func(f *desc.FieldDescriptor) []lint.Problem {
+		want := "uid"
+		target := "id"
+		if f.GetName() == target {
+			return []lint.Problem{{
+				Message:    fmt.Sprintf("Use %s instead of %s.", want, target),
+				Descriptor: f,
+				Location:   locations.DescriptorName(f),
+				Suggestion: want,
+			}}
+		}
+		return nil
+	},
+}
