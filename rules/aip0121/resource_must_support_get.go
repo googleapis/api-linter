e7236687b64c1e465ce03048c8b67fe09db1091b
Noah Dietz <noahdietz@users.noreply.github.com>
2023-09-12 09:20:40 -0700

    fix(AIP-121): ignore streaming lookalikes (#1252)

--- rules/aip0121/resource_must_support_get.go
+++ rules/aip0121/resource_must_support_get.go
@@ -33,6 +33,12 @@ var resourceMustSupportGet = &lint.ServiceRule{
 		// Iterate all RPCs and try to find resources. Mark the
 		// resources which have a Get method, and which ones do not.
 		for _, m := range s.GetMethods() {
+			// Streaming methods do not count as standard methods even if they
+			// look like them.
+			if utils.IsStreaming(m) {
+				continue
+			}
+
 			if utils.IsGetMethod(m) && utils.IsResource(utils.GetResponseType(m)) {
 				t := utils.GetResource(m.GetOutputType()).GetType()
 				resourcesWithGet.Add(t)
