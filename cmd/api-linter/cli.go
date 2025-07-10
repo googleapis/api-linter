80ffc33d8c744f4f91b76929438aa15bd23fe44c
Santiago Quiroga <22756465+quirogas@users.noreply.github.com>
2025-07-10 16:24:37 -0700

    Update cmd/api-linter/cli.go
    Co-authored-by: Noah Dietz <noahdietz@users.noreply.github.com>

--- cmd/api-linter/cli.go
+++ cmd/api-linter/cli.go
@@ -329,8 +329,9 @@ func resolveImports(protoImportPaths []string) []string {
 
 	// Initialize resolvedImports with "." and track its canonical absolute path.
 	resolvedImports := []string{"."}
-	seenAbsolutePaths := make(map[string]bool)
-	seenAbsolutePaths[evaluatedCwd] = true // Mark canonical CWD as seen
+	seenAbsolutePaths := map[string]bool{
+	  evaluatedCwd: true, // Mark canonical CWD as seen
+	}
 
 	for _, p := range protoImportPaths {
 		absPath, err := filepath.Abs(p)
