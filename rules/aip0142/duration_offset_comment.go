4f5418eb9084c9bcf553cd1363cb38e897c63ef4
Santiago Quiroga <santiquiroga@google.com>
2025-07-15 14:17:00 -0700

    chore(aip0142): Add rule for durration offset comment

--- rules/aip0142/duration_offset_comment.go
+++ rules/aip0142/duration_offset_comment.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,27 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package aip0142 contains rules defined in https://aip.dev/142.
 package aip0142
 
 import (
+	"strings"
+
 	"github.com/googleapis/api-linter/lint"
 	"github.com/googleapis/api-linter/rules/internal/utils"
 	"github.com/jhump/protoreflect/desc"
 )
 
-// AddRules adds all of the AIP-142 rules to the provided registry.
-func AddRules(r lint.RuleRegistry) error {
-	return r.Register(
-		142,
-		fieldNames,
-		fieldType,
-		durationOffsetComment,
-	)
-}
-
-// isTimestamp simply returns if the field is of type google.protobuf.Timestamp
-// or not.
-func isTimestamp(f *desc.FieldDescriptor) bool {
-	return utils.GetTypeName(f) == "google.protobuf.Timestamp"
+var durationOffsetComment = &lint.FieldRule{
+	Name: lint.NewRuleName(142, "duration-offset-comment"),
+	OnlyIf: func(f *desc.FieldDescriptor) bool {
+		return utils.GetTypeName(f) == "google.protobuf.Duration" && strings.HasSuffix(f.GetName(), "_offset")
+	},
+	LintField: func(f *desc.FieldDescriptor) []lint.Problem {
+		comment := f.GetSourceInfo().GetLeadingComments()
+		if comment == "" {
+			return []lint.Problem{{
+				Message:    "Duration fields ending in `_offset` must include a comment explaining the relative start point.",
+				Descriptor: f,
+			}}
+		}
+		return nil
+	},
 }
