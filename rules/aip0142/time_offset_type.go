945cff325fe38d4f8e3a7a620059a1e6b04c5107
Santiago Quiroga <22756465+quirogas@users.noreply.github.com>
2025-07-16 16:02:44 -0700

    feat(AIP-142): add time_offset type rule (#1506)
    * feat(aip0142): add time_offset type rule
    
    * Update rules/aip0142/time_offset_type.go
    
    Co-authored-by: Noah Dietz <noahdietz@users.noreply.github.com>
    
    * Update rules/aip0142/time_offset_type_test.go
    
    Co-authored-by: Noah Dietz <noahdietz@users.noreply.github.com>
    
    * chore(aip0142): add rule to trigger mechanism
    
    * chore(aip0142): update rule name
    
    ---------
    
    Co-authored-by: Noah Dietz <noahdietz@users.noreply.github.com>

--- rules/aip0142/time_offset_type.go
+++ rules/aip0142/time_offset_type.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,28 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package aip0142 contains rules defined in https://aip.dev/142.
 package aip0142
 
 import (
+	"strings"
+
 	"github.com/googleapis/api-linter/lint"
+	"github.com/googleapis/api-linter/locations"
 	"github.com/googleapis/api-linter/rules/internal/utils"
 	"github.com/jhump/protoreflect/desc"
 )
 
-// AddRules adds all of the AIP-142 rules to the provided registry.
-func AddRules(r lint.RuleRegistry) error {
-	return r.Register(
-		142,
-		fieldNames,
-		fieldType,
-		durationOffsetComment,
-		timeOffsetType,
-	)
-}
-
-// isTimestamp simply returns if the field is of type google.protobuf.Timestamp
-// or not.
-func isTimestamp(f *desc.FieldDescriptor) bool {
-	return utils.GetTypeName(f) == "google.protobuf.Timestamp"
+var timeOffsetType = &lint.FieldRule{
+	Name: lint.NewRuleName(142, "time-offset-type"),
+	LintField: func(f *desc.FieldDescriptor) []lint.Problem {
+		if utils.GetTypeName(f) != "google.protobuf.Duration" && strings.HasSuffix(f.GetName(), "_time_offset") {
+			return []lint.Problem{{
+				Message:    "Fields with names ending in `_time_offset` must be of type `google.protobuf.Duration`.",
+				Descriptor: f,
+				Suggestion: "google.protobuf.Duration",
+				Location:   locations.FieldType(f),
+			}}
+		}
+		return nil
+	},
 }
