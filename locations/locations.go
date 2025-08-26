ebbc67e62b4dec391132de278488cea50a054df7
Santiago Quiroga <santiquiroga@google.com>
2025-08-26 10:39:05 -0700

    chore(locations): simplify internal logic

--- locations/locations.go
+++ locations/locations.go
@@ -33,70 +33,11 @@ import (
 // pathLocation returns the precise location for a given descriptor and path.
 // It combines the path of the descriptor itself with any path provided appended.
 func pathLocation(d protoreflect.Descriptor, path ...int) *dpb.SourceCodeInfo_Location {
-	fullPath := getDescriptorPath(d)
+	fullPath := d.ParentFile().SourceLocations().ByDescriptor(d).Path
 	for _, i := range path {
 		fullPath = append(fullPath, int32(i))
 	}
 	return sourceInfoRegistry.sourceInfo(d.ParentFile()).findLocation(fullPath)
-}
-
-// getDescriptorPath returns the path for a given descriptor.
-//
-// This function is necessary because the modern `protoreflect` API does not
-// provide a way to get this path directly from a descriptor, unlike the older
-// `jhump/protoreflect` library. While `SourceLocations.ByDescriptor()` can find
-// the location for an entire descriptor, it does not provide the `[]int32` path
-// itself. This path is the essential "key" needed to look up the locations of
-// a descriptor's sub-elements (e.g., a field's name vs. its type).
-//
-// This function reconstructs the path by traversing upwards from the descriptor
-// to the file root, collecting the field numbers and indexes at each step.
-func getDescriptorPath(d protoreflect.Descriptor) []int32 {
-	var path []int32
-	current := d
-	for current != nil {
-		var fieldNumber int32
-		parent := current.Parent()
-		switch desc := current.(type) {
-		case protoreflect.FileDescriptor:
-			// Done.
-			current = nil
-			continue
-		case protoreflect.MessageDescriptor:
-			if _, ok := parent.(protoreflect.FileDescriptor); ok {
-				fieldNumber = 4 // FileDescriptorProto.message_type
-			} else { // parent is MessageDescriptor (nested)
-				fieldNumber = 3 // DescriptorProto.nested_type
-			}
-		case protoreflect.FieldDescriptor:
-			if desc.IsExtension() {
-				if _, ok := parent.(protoreflect.FileDescriptor); ok {
-					fieldNumber = 7 // FileDescriptorProto.extension
-				} else { // parent is MessageDescriptor (nested extension)
-					fieldNumber = 6 // DescriptorProto.extension
-				}
-			} else {
-				fieldNumber = 2 // DescriptorProto.field
-			}
-		case protoreflect.OneofDescriptor:
-			fieldNumber = 8 // DescriptorProto.oneof_decl
-		case protoreflect.EnumDescriptor:
-			if _, ok := parent.(protoreflect.FileDescriptor); ok {
-				fieldNumber = 5 // FileDescriptorProto.enum_type
-			} else { // parent is MessageDescriptor (nested)
-				fieldNumber = 4 // DescriptorProto.enum_type
-			}
-		case protoreflect.EnumValueDescriptor:
-			fieldNumber = 2 // EnumDescriptorProto.value
-		case protoreflect.ServiceDescriptor:
-			fieldNumber = 6 // FileDescriptorProto.service
-		case protoreflect.MethodDescriptor:
-			fieldNumber = 2 // ServiceDescriptorProto.method
-		}
-		path = append([]int32{fieldNumber, int32(current.Index())}, path...)
-		current = parent
-	}
-	return path
 }
 
 type sourceInfo struct {
